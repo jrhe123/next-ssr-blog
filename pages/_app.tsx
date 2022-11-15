66591063f5dae82838d2aed38cf7990fb9a47195
jrhe1213 <jiarong1213@gmail.com>
2022-11-14 21:14:12 -0500

    use local font

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,19 @@
-import '../styles/globals.css';
-import type { AppProps } from 'next/app';
-import Layout from '../components/Layout';
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import Layout from "../components/Layout";
+// font
+import localFont from "@next/font/local";
+
+const myfont = localFont({
+  src: "../fonts/Pacifico-Regular.woff2",
+});
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <main className={myfont.className}>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </main>
   );
 }
