e4463e5d4f3722ac427100927b3a4521cd3024fe
jrhe1213 <jiarong1213@gmail.com>
2022-11-22 23:39:10 -0500

    notes

---
+++ notes/getInitialProps.md
@@ -0,0 +1,22 @@
+// Tag.getInitialProps = wrapper.getInitialPageProps(
+//   (store) =>
+//     async ({ pathname, req, res }) => {
+//       console.log("+++++++ tag page getInitialPageProps");
+//       console.log("+++++++ tag page getInitialPageProps");
+//       console.log("+++++++ tag page getInitialPageProps");
+//       console.log("+++++++ tag page getInitialPageProps");
+//       console.log("+++++++ tag page getInitialPageProps");
+//       console.log("+++++++ tag page getInitialPageProps");
+//       // console.log("+++++++ req: ", req);
+//       // console.log("+++++++ res: ", res);
+//       // console.log('2. Page.getInitialProps uses the store to dispatch things');
+//       // store.dispatch({
+//       //   type: 'TICK',
+//       //   payload: 'was set in error page ' + pathname,
+//       // });
+//       return {
+//         props: {},
+//       };
+//     }
+// );
+// // https://github.com/kirill-konshin/next-redux-wrapper#pagegetinitialprops
--- pages/tag/index.tsx
+++ pages/tag/index.tsx
@@ -21,27 +21,5 @@ export const getServerSideProps = wrapper.getServerSideProps(
       };
     }
 );
-// Tag.getInitialProps = wrapper.getInitialPageProps(
-//   (store) =>
-//     async ({ pathname, req, res }) => {
-//       console.log("+++++++ tag page getInitialPageProps");
-//       console.log("+++++++ tag page getInitialPageProps");
-//       console.log("+++++++ tag page getInitialPageProps");
-//       console.log("+++++++ tag page getInitialPageProps");
-//       console.log("+++++++ tag page getInitialPageProps");
-//       console.log("+++++++ tag page getInitialPageProps");
-//       // console.log("+++++++ req: ", req);
-//       // console.log("+++++++ res: ", res);
-//       // console.log('2. Page.getInitialProps uses the store to dispatch things');
-//       // store.dispatch({
-//       //   type: 'TICK',
-//       //   payload: 'was set in error page ' + pathname,
-//       // });
-//       return {
-//         props: {},
-//       };
-//     }
-// );
-// // https://github.com/kirill-konshin/next-redux-wrapper#pagegetinitialprops
 
 export default Tag;
