6add12c47b38b6d1cc8585cea8200a5bb00ac6ed
jrhe1213 <jiarong1213@gmail.com>
2022-11-27 20:14:49 -0500

    add server.js for custom server config

---
+++ server.js
@@ -0,0 +1,28 @@
+const { createServer } = require("http");
+const { parse } = require("url");
+const next = require("next");
+
+const dev = process.env.NODE_ENV !== "production";
+const hostname = "localhost";
+const port = 4000;
+
+const app = next({ dev, hostname, port });
+const handle = app.getRequestHandler();
+
+app.prepare().then(() => {
+  createServer(async (req, res) => {
+    try {
+      const parsedUrl = parse(req.url, true);
+      const { pathname, query } = parsedUrl;
+      //
+      await handle(req, res, parsedUrl);
+    } catch (err) {
+      console.log("error", req.url, err);
+      res.statusCode = 500;
+      res.end("internal server error");
+    }
+  }).listen(port, (err) => {
+    if (err) throw err;
+    console.log(`Server is running at：http://${hostname}:${port}`);
+  });
+});
